947b060ad6f0792b32964609130a87bbb89c2cf5
Valentin Panasyuk <wpanalex@gmail.com>
2023-08-10 17:55:31 +0300

    fix parser size

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRouter = require('./authRouter.js');
+const bodyParser = require('body-parser')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json());
 app.use('/auth', authRouter);
 const start = async () => {
